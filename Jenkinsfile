ac14296560399805dc9208226fc9fec61e849dbd
bence <borbelybence98@gmail.com>
2020-05-28 14:45:35 +0200

    exception handling added

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                sh './main.sh 150'
+                sh './main.sh'
             }
         }
     }
--- main.sh
+++ main.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 
-loadList () {
-    for ((i=2; i <=$1 ;i++)); do
-        all_numbers+=($i)
-        done
-    checkPrimes
-}
-
 checkPrimes () {
-    len=${#all_numbers[@]}
-    
-    for (( i=0; i <=$len; i++ )); do
+    for (( i=2; i <=$max_number; i++ )); do
         numFac=`factor $i | wc -w`
         if [[ $numFac -lt 3 ]]; then
             primes+=($i)
         fi
     done
+    
 }
 
 max_number=$1
-declare -a all_numbers;
 declare -a primes;
-loadList $max_number
-echo "Primes till" $max_number "are :" ${primes[*]}
+
+if [[ $max_number -gt 0 ]]; then
+    checkPrimes;
+    echo "Primes till" $max_number "are :" ${primes[*]};
+else
+    echo "Execute the program with a number parameter."
+fi
+
+
 
